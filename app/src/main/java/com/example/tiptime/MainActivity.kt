a2357f744ecf2e3bb39d5810129fff30a93e5699
Joshua Y Jo <125523585+antimonyc@users.noreply.github.com>
2024-09-12 07:56:47 -0500

    finished tip app

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,10 +19,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.annotation.DrawableRes
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
@@ -30,6 +37,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.Switch
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
@@ -60,13 +69,14 @@ fun TipTimeLayout() {
         // every time the variables change
         var amountInput by remember { mutableStateOf("") }
         var tipInput by remember { mutableStateOf("") }
+        var roundUp by remember { mutableStateOf(false) }
 
         // elvis operator used
         // if value is null, returns 0.0
         // if value is NOT null, then return the actual value
         val amount = amountInput.toDoubleOrNull() ?: 0.0
         val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
-        val tip = calculateTip(amount, tipPercent)
+        val tip = calculateTip(amount, tipPercent, roundUp)
 
         // create a column that will store multiple items
         Column(
@@ -74,7 +84,8 @@ fun TipTimeLayout() {
             modifier = Modifier
                 .statusBarsPadding()
                 .padding(horizontal = 40.dp)
-                .safeDrawingPadding(),
+                .safeDrawingPadding()
+                .verticalScroll(rememberScrollState()),
             // horizontalAlignment and verticalArrangement can be used
             // together
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -95,6 +106,7 @@ fun TipTimeLayout() {
                 // before anyone types in there
                 // label = "Bill Amount"
                 label = R.string.bill_amount,
+                leadingIcon = R.drawable.money,
                 keyboardOptions = KeyboardOptions.Default.copy(
                     keyboardType = KeyboardType.Number,
                     imeAction = ImeAction.Next
@@ -109,8 +121,8 @@ fun TipTimeLayout() {
                 onValueChange = { amountInput = it },
                 // modifiers for modifiers
                 modifier = Modifier
-                .padding(bottom = 32.dp)
-                .fillMaxWidth()
+                    .padding(bottom = 32.dp)
+                    .fillMaxWidth()
             )
             // create a second number field for the tip percentage
             EditNumberField(
@@ -118,6 +130,7 @@ fun TipTimeLayout() {
                 // before anyone types in there
                 // label = "Tip Percentage"
                 label = R.string.how_was_the_service,
+                leadingIcon = R.drawable.percent,
                 keyboardOptions = KeyboardOptions.Default.copy(
                     keyboardType = KeyboardType.Number,
                     imeAction = ImeAction.Done
@@ -132,6 +145,11 @@ fun TipTimeLayout() {
                 modifier = Modifier
                     .padding(bottom = 32.dp)
                     .fillMaxWidth()
+            )
+            RoundTheTipRow(
+                roundUp = roundUp,
+                onRoundUpChanged = { roundUp = it },
+                modifier = Modifier.padding(bottom = 32.dp)
             )
             // create a text area that prints out the result
             // which is the tip amount
@@ -153,6 +171,7 @@ fun EditNumberField(
     // it is labeled as a "StringRes", meaning String Resource
     // it is represented by an Int
     @StringRes label: Int,
+    @DrawableRes leadingIcon: Int,
     keyboardOptions: KeyboardOptions,
     value: String,
     onValueChange: (String) -> Unit,
@@ -160,6 +179,7 @@ fun EditNumberField(
 {
     TextField(
         label = { Text(stringResource(label)) },
+        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null)},
         singleLine = true,
         keyboardOptions = keyboardOptions,
         value = value,
@@ -173,9 +193,35 @@ fun EditNumberField(
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0,
+                         roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@Composable
+fun RoundTheTipRow(modifier: Modifier = Modifier,
+                   onRoundUpChanged: (Boolean) -> Unit,
+                   roundUp: Boolean) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.round_up_tip))
+
+        Switch(
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged,
+        )
+    }
 }
 
 @Preview(showBackground = true)
